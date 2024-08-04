0ef6bce9d91215ce38ad186736f82a5e98554b91
Mimika <mimika.routes@gmail.com>
2024-08-04 21:43:42 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,10 +3,22 @@ import json
 
 API_KEY = 'JHFa6EPnJSGkGPoHs9bxPky5dXIAAz36'
 QUERY_ID = '3930803'
-BASE_URL = 'https://api.dune.com/api/v1/query/'
 
-response = requests.get(f'{BASE_URL}{QUERY_ID}/results', headers={'x-dune-api-key': API_KEY})
+url = f'https://api.dune.com/api/v1/query/{QUERY_ID}/results'
+headers = {
+    'x-dune-api-key': API_KEY
+}
+
+response = requests.get(url, headers=headers)
 data = response.json()
 
+
+results = data.get('data', {}).get('result', [])
+output = {}
+for result in results:
+    badge_name = result['badge_name']
+    minted_percentage = result['minted_percentage']
+    output[badge_name] = minted_percentage
+
 with open('data.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(output, f)
