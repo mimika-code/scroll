4a02195f2ecead9a17ab8425841666641d82fb2f
Mimika <mimika.routes@gmail.com>
2024-08-05 02:05:56 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,36 +1,23 @@
-import requests
+from dune_client.client import DuneClient
 import json
 
 API_KEY = 'JHFa6EPnJSGkGPoHs9bxPky5dXIAAz36'
-QUERY_ID = '3930803'
+QUERY_ID = 3930803
 
-url = f'https://api.dune.com/api/v1/query/{QUERY_ID}/results'
-headers = {
-    'x-dune-api-key': API_KEY
-}
+dune = DuneClient(API_KEY)
+query_result = dune.get_latest_result(QUERY_ID)
 
-response = requests.get(url, headers=headers)
-
-print("Status code:", response.status_code)
-if response.status_code != 200:
-    print("Error:", response.text)
-else:
-    data = response.json()
-    print("Received data:", data)
-
-    results = data.get('result', {}).get('rows', [])
-    output = {}
-    for result in results:
-        badge_name = result['badge_name']
-        minted_percentage = result['share']
-        output[badge_name] = minted_percentage
-
+if query_result:
+    results = query_result['result']['rows']
+    output = {result['badge_name']: result['share'] for result in results}
+    
     print("Output data to be written to data.json:", output)
 
     with open('data.json', 'w') as f:
         json.dump(output, f)
 
-  
     with open('data.json', 'r') as f:
         content = f.read()
         print("Content of data.json:", content)
+else:
+    print("No results found")
